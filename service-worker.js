f4c085e51eeb36e779cb77dd63c13f4c8b3e7551
woutvanveldhuizen <woutvanveldhuizen@gmail.com>
2026-07-01 22:54:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Vakantie-uitgaven service worker
-const CACHE_NAME = "vakantie-uitgaven-v2-1-2-koersfix";
+const CACHE_NAME = "vakantie-uitgaven-v2-1-2-iconen-1";
 const APP_FILES = ["./","index.html","manifest.webmanifest","icons/icon-180.png","icons/icon-192.png","icons/icon-512.png"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):null))).then(()=>self.clients.claim()))});
